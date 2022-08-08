874059ef9a1a1ad7e586c9c3832c1a0284677d60
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-07 23:23:04 -0400

    create assets folder continue html and script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+var startbtn=document.querySelector("button")
+console.log(startbtn)
+var listOfQuestions=[
+    {
+        question:"question1",
+        choiceA:"choiceA",
+        choiceB:"choiceB",
+        choiceC:"choiceC",
+        choiceD:"choiceD",
+        answer:"choiceD"
+
+    },
+    {
+        question:"question2",
+        choiceA:"choiceA",
+        choiceB:"choiceB",
+        choiceC:"choiceC",
+        choiceD:"choiceD",
+        answer:"choiceD"
+        
+    }
+
+
+]
+function showQuestion(){
+console.log(listOfQuestions[0])
+var questionTitleEl=document.getElementById("questionTitle")
+questionTitleEl.innerText=listOfQuestions[0]
+}
+function startGame(){
+  showQuestion()
+}
+startbtn.addEventListener("click",startGame)
